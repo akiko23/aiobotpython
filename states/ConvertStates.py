68e82a9a08eb42c5e3733f5049edf5093979603d
akiko23 <101889433+akiko23@users.noreply.github.com>
2022-06-26 11:24:04 +0300

    Delete states directory

--- states/ConvertStates.py
+++
@@ -1,5 +0,0 @@
-from aiogram.dispatcher.filters.state import StatesGroup, State
-
-
-class Convert(StatesGroup):
-    c1 = State()
--- states/Pay_sub.py
+++
@@ -1,5 +0,0 @@
-from aiogram.dispatcher.filters.state import StatesGroup, State
-
-class BuySubscribe(StatesGroup):
-    payment_choice = State()
-    check_payment = State()
